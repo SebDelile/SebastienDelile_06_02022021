63d4bcc6d6350b69bbb5de9657d53209fbaf995d
Delile Sebastien <sebastiendelile@gmail.com>
2021-02-09 13:06:30 +0100

    JS open and close modale features
    ajout de la fonction d'ouverture et fermeture des modale.
    quelques modifs dans le CSS pour rendre les animationsfonctionnelles

--- JS/main_photographer.js
+++ JS/main_photographer.js
@@ -1,13 +1,33 @@
 //--------------------------------------------------------------------------------------------
 //------------------------------ Import from modules -----------------------------------------
 //--------------------------------------------------------------------------------------------
+import { openModal, closeModal } from "./common/openCloseModal.js";
 
+//--------------------------------------------------------------------------------------------
+//----------------------------------- DOM elements -------------------------------------------
+//--------------------------------------------------------------------------------------------
 
-
+const buttonContact = document.querySelector(".profile__contact");
+const medias = document.getElementsByClassName("media");
+const formModal = document.querySelector(".form__modal");
+const lightboxModal = document.querySelector(".lightbox__modal");
 
 //--------------------------------------------------------------------------------------------
 //--------------------------------- Event listeners ------------------------------------------
 //--------------------------------------------------------------------------------------------
 
+buttonContact.addEventListener("click", function () {
+  openModal(formModal);
+});
+for (let media of medias) {
+  media.addEventListener("click", function () {
+    openModal(lightboxModal);
+  });
+}
 
-
+formModal.querySelector(".form__close").addEventListener("click", function(){
+    closeModal(formModal);
+});
+lightboxModal.querySelector(".lightbox__close").addEventListener("click", function(){
+    closeModal(lightboxModal);
+});
