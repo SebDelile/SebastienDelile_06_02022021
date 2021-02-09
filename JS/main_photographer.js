d685e6eff1dc8c939678029b5cb1edfa1cbc3eee
Delile Sebastien <sebastiendelile@gmail.com>
2021-02-09 19:21:52 +0100

    JS form feature
    fait le javascript pour le formulaire, vérification HTML5 et soumission avec console.log. Pas de message personnalisé
    Pas mal de changement dans le HTML et CSS pour s'adapter

--- JS/main_photographer.js
+++ JS/main_photographer.js
@@ -1,7 +1,8 @@
 //--------------------------------------------------------------------------------------------
 //------------------------------ Import from modules -----------------------------------------
 //--------------------------------------------------------------------------------------------
-import { openModal, closeModal } from "./common/openCloseModal.js";
+import {openModal, closeModal} from "./common/openCloseModal.js";
+import {formValidity, formSubmission, submissionConfirmation} from "./page_photographer/contact_form.js"
 
 //--------------------------------------------------------------------------------------------
 //----------------------------------- DOM elements -------------------------------------------
@@ -11,11 +12,13 @@ const buttonContact = document.querySelector(".profile__contact");
 const medias = document.getElementsByClassName("media");
 const formModal = document.querySelector(".form__modal");
 const lightboxModal = document.querySelector(".lightbox__modal");
+const form = document.querySelector("form");
 
 //--------------------------------------------------------------------------------------------
 //--------------------------------- Event listeners ------------------------------------------
 //--------------------------------------------------------------------------------------------
 
+//------------------------------ Open/Close the modale ---------------------------------------
 buttonContact.addEventListener("click", function () {
   openModal(formModal);
 });
@@ -24,10 +27,21 @@ for (let media of medias) {
     openModal(lightboxModal);
   });
 }
-
 formModal.querySelector(".form__close").addEventListener("click", function(){
     closeModal(formModal);
 });
 lightboxModal.querySelector(".lightbox__close").addEventListener("click", function(){
     closeModal(lightboxModal);
+});
+
+//------------------------------ form Verification/Submission --------------------------------
+for (let field of form) {
+    field.addEventListener("input", function(event) {
+        formValidity(event.target);
+    });
+};
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    formSubmission(event.target);
+    submissionConfirmation(event.target);
 });
--- JS/page_photographer/contact_form.js
+++ JS/page_photographer/contact_form.js
@@ -3,16 +3,39 @@
 //--------------------------------------------------------------------------------------------
 
 
-
-
-
 //--------------------------------------------------------------------------------------------
 //------------------------------- Intermediate stages ----------------------------------------
 //--------------------------------------------------------------------------------------------
 
 
 
-
 //--------------------------------------------------------------------------------------------
 //----------------------------------- Export(s) ----------------------------------------------
 //--------------------------------------------------------------------------------------------
+
+export function formValidity (field){
+    if (field.validity.valid) {
+        field.style.border = "0.25rem solid #DB8876"; /*color secondary*/
+        field.style.borderRadius = "calc(0.3215rem + 0.25rem)" /*need to take into account the border thickness*/
+        field.parentNode.querySelector(".form__validity").setAttribute("src", "public/img/icon/checked.svg");
+    }
+    else {
+        field.style.border = "0.25rem solid #901C1C"; /*color primary*/
+        field.style.borderRadius = "0.3215rem"
+        field.parentNode.querySelector(".form__validity").setAttribute("src", "public/img/icon/warning.svg");
+    }
+}
+
+
+export function formSubmission(form){
+    for (let field of form) {
+        if (field.type !== "submit") {
+            console.log(`${field.name} : ${field.value}`);
+        }
+    }
+}
+
+export function submissionConfirmation(form) {
+    form.querySelector(".form__submitted").style.display = "flex";
+    form.querySelector(".form__submitted p").style.animation = "fade-shift 600ms both";
+}
