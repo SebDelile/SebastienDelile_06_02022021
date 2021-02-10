25552164d4f4e98133142d9319b45414166a0ba3
Delile Sebastien <sebastiendelile@gmail.com>
2021-02-10 14:02:05 +0100

    JS index card generator feature
    fait la fonction qui importe les photographes depuis le json et créé les éléments sur la page index.dossier de photo renommé et une faute de frappe corrigée dans un nom de fichier

--- JS/page_index/card_generator.js
+++ JS/page_index/card_generator.js
@@ -2,17 +2,29 @@
 //------------------------------ Import from modules -----------------------------------------
 //--------------------------------------------------------------------------------------------
 
-
-
-
-
 //--------------------------------------------------------------------------------------------
 //------------------------------- Intermediate stages ----------------------------------------
 //--------------------------------------------------------------------------------------------
 
-
-
-
 //--------------------------------------------------------------------------------------------
 //----------------------------------- Export(s) ----------------------------------------------
 //--------------------------------------------------------------------------------------------
+
+export function createCard(photographer) {
+  let element = document.createElement("article");
+  element.classList.add("card");
+  element.setAttribute("id", `id${photographer.id}`);
+  element.innerHTML = `
+    <a href="photographer.html?id${photographer.id}">
+     <img class="card__picture" src="./public/img/photographer-portraits/${photographer.portrait}" alt="photographer profile picture" />
+     <h3 class="card__name">${photographer.name}</h3>
+     <p class="card__location">${photographer.city}, ${photographer.country}</p>
+     <p class="card__quote">${photographer.tagline}</p>
+     <p class="card__price">${photographer.price} €/j</p>
+    </a>
+    <ul class="card__taglist"></ul>`;
+  for (let tag in photographer.tags) {
+    element.querySelector(".card__taglist").insertAdjacentHTML("beforeend", `<li class="card__tag tag"><a href="${photographer.tags[tag]}">#${photographer.tags[tag]}</a></li>`);
+    document.querySelector(".main__grid").append(element);
+  };
+}
